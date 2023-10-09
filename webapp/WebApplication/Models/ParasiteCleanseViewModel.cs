45361529a8b3fc65c7f38d983b158c2714ea57bc
= <=>
2023-10-09 15:28:59 +0700

    sg - update

---
+++ webapp/WebApplication/Models/ParasiteCleanseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using K9.DataAccessLayer.Models;
+
+namespace K9.WebApplication.Models
+{
+    public class ParasiteCleanseViewModel
+    {
+        public ProtocolProduct ParasiteCleanseProduct { get; set; }
+        public ProtocolProduct ParasiteCleanse2Product { get; set; }
+        public ProtocolProduct ParasiteCleanse3Product { get; set; }
+        public int[] ParasiteCleanseDosages { get; set; }
+        public int[] ParasiteCleanse2Dosages { get; set; }
+        public int[] ParasiteCleanse3Dosages { get; set; }
+    }
+}
