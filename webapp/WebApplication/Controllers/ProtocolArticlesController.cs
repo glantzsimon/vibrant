6c16db80eccc2806354ec9e59f258fd997369bb3
= <=>
2023-10-23 02:14:56 +0700

    sg - update

--- webapp/WebApplication/Controllers/ProtocolArticlesController.cs
+++ webapp/WebApplication/Controllers/ProtocolArticlesController.cs
@@ -25,41 +25,6 @@ namespace K9.WebApplication.Controllers
         {
             return View(_protocolService.List(true));
         }
-
-        [Route("health-questionnaire")]
-        public ActionResult Questionnaire()
-        {
-            return View(); ;
-        }
-
-        [Route("cbs-upregulation")]
-        public ActionResult CbsUpregulation()
-        {
-            return View(); ;
-        }
-
-        [Route("sulfur-detox")]
-        public ActionResult SulfurDetox()
-        {
-            return View(); ;
-        }
-
-        [Route("methylation-support")]
-        public ActionResult MethylationSupport()
-        {
-            return View(); ;
-        }
-
-        [Route("heavy-metal-detox")]
-        public ActionResult HeavyMetalDetox()
-        {
-            return View(); ;
-        }
-
-        [Route("general-wellbeing")]
-        public ActionResult GeneralWellbeing()
-        {
-            return View(); ;
-        }
+        
     }
 }
