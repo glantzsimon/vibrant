4ddf842b4d26a224a679d148da0886831e7eccd4
= <=>
2023-10-23 14:34:43 +0700

    sg - update

--- webapp/DataAccess/Helpers/Methods.cs
+++ webapp/DataAccess/Helpers/Methods.cs
@@ -55,7 +55,7 @@ namespace K9.DataAccessLayer.Helpers
 
         public static double GetSuggestedBulkDiscount(this double price)
         {
-            return price <= 10000 ? 0 : 1 - (10000 / price) * 0.1;
+            return price <= 10000 ? 0 : (1 - (10000 / price)) * 0.1;
         }
 
         public static double GetSuggestedBulkDiscountPrice(this double price)
