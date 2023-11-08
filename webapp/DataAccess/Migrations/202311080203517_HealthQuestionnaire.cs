9bee7a58e4e6e67dcfadb81ebb7b82b76d84742d
= <=>
2023-11-08 09:04:18 +0700

    sg - update

---
+++ webapp/DataAccess/Migrations/202311080203517_HealthQuestionnaire.cs
@@ -0,0 +1,20 @@
+namespace K9.DataAccessLayer.Database
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class HealthQuestionnaire : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Protocol", "ProtocolFrequency", c => c.Int(nullable: false));
+            DropColumn("dbo.Protocol", "Frequency");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Protocol", "Frequency", c => c.Int(nullable: false));
+            DropColumn("dbo.Protocol", "ProtocolFrequency");
+        }
+    }
+}
