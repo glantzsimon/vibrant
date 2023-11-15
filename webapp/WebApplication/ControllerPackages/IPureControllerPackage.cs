c461bb73c941f5618ac4d9ea8ce5f69804eb1ffc
= <=>
2023-11-15 22:07:01 +0700

    sg - update

---
+++ webapp/WebApplication/ControllerPackages/IPureControllerPackage.cs
@@ -0,0 +1,10 @@
+﻿using K9.WebApplication.Services;
+
+namespace K9.WebApplication.Packages
+{
+    public class PureControllerPackage : IPureControllerPackage
+    {
+        public IShoppingCartService ShoppingCartService { get; set; }
+        public IMembershipService MembershipService { get; set; }
+    }
+}
---
+++ webapp/WebApplication/ControllerPackages/PureControllerPackage.cs
@@ -0,0 +1,10 @@
+﻿using K9.WebApplication.Services;
+
+namespace K9.WebApplication.Packages
+{
+    public interface IPureControllerPackage
+    {
+        IShoppingCartService ShoppingCartService { get; set; }
+        IMembershipService MembershipService { get; set; }
+    }
+}
