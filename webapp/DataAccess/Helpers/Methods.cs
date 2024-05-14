df9b845afd8466d9852d5a79be8b0917ac88d04e
= <=>
2024-05-14 21:55:56 +0700

    sg - update

--- webapp/DataAccess/Helpers/Methods.cs
+++ webapp/DataAccess/Helpers/Methods.cs
@@ -75,9 +75,11 @@ namespace K9.DataAccessLayer.Helpers
             return string.Empty;
         }
 
-        public static string SelectLines(this string value, int numberOfLines)
+        public static string SelectLines(this string value, int numberOfLines, bool isUnix = false)
         {
-            return string.Join(Environment.NewLine, Regex.Split(value, Environment.NewLine).Select(e => e.Trim()).Where(e => !string.IsNullOrEmpty(e) && !string.IsNullOrWhiteSpace(e)).Take(numberOfLines));
+            var output = string.Join(isUnix ? "\n" : Environment.NewLine, Regex.Split(value, Environment.NewLine).Select(e => e.Trim()).Where(e => !string.IsNullOrEmpty(e) && !string.IsNullOrWhiteSpace(e)).Take(numberOfLines));
+
+            return output;
         }
 
         public static string ToCurrency(this double value, string currencyFormat = "฿")
